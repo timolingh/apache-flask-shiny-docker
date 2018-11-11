1114631e3b5fee765f18f6b637d95be61ccedb47
timolingh <timolin@gmail.com>
2018-11-11 14:27:37 -0800

    Create a variable app list

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,20 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    apps = [
+    {'appname':'simpleportfoliomc', 'label':'Simple portfolio MC simulator'},
+    ]
+
+    return render_template('index.html', apps=apps)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/shiny')
+@app.route('/shiny/')
 def shiny():
-    return render_template('shiny.html')
+    # Get the app name from the template - the 'shinyapp' arg in 'url_for()'
+    myvar = request.args.get('shinyapp', "")
+
+    # Uses the value of 'myvar' to build the url for the Shiny app in 'shiny.html'
+    return render_template('shiny.html', shinyapp=myvar)
