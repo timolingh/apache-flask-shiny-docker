761ac51a5bea7702cd40dca685be4ce68fa0d2d7
Lin <lint1@loam002590280.corp.amvescap.net>
2018-11-12 12:52:46 -0800

    Parameter changes for EC2

--- app/routes.py
+++ app/routes.py
@@ -6,16 +6,30 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    apps = [
-    {'appname':'simpleportfoliomc', 'label':'Simple portfolio MC simulator'},
+
+    EC2ADDRESS = '10.0.0.150'
+
+    DASHBOARDAPPS = [
+    {'appname':'dashboard_app',         'label':'Jemstep AUM'},
+    {'appname':'event_stream',          'label':'Platform user events'},
+    {'appname':'engineering_delivery',  'label':'Code delivery metrics'}
     ]
 
-    return render_template('index.html', apps=apps)
+    SHINYAPPS = [
+    {'appname':'montecarloreturns',     'label':'Simple portfolio MC simulator'},
+    ]
+
+    return render_template('index.html',
+                           ec2address=EC2ADDRESS,
+                           dashboardapps=DASHBOARDAPPS,
+                           shinyapps = SHINYAPPS
+                           )
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+## Not currently used
 @app.route('/shiny/')
 def shiny():
     # Get the app name from the template - the 'shinyapp' arg in 'url_for()'
